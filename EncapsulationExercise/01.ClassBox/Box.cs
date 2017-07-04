25a50e86d9e9ecc9bc4db8ff0d891ece1601fa60
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-05 00:38:29 +0300

    Second - 75/100

---
+++ EncapsulationExercise/01.ClassBox/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ClassBox
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            this.Lenght = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Lenght
+        {
+            get
+            {
+                return this.lenght;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Lenght cannot be zero or negative.");
+                }
+                this.lenght = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * this.lenght * this.width + 2 * this.lenght * this.height + 2 * this.width * this.height;
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * this.lenght* this.height + 2 * this.width * this.height;
+        }
+        public double Volume()
+        {
+            return this.lenght * this.width * this.height;
+        }
+    }
+}
---
+++ EncapsulationExercise/01.ClassBox/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ClassBox
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            try
+            {
+                Box box = new Box(lenght, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
