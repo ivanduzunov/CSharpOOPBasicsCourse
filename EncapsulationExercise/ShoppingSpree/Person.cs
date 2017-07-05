addd3a2f9a8e9afc02fbf098aad03ec2d88e824d
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-05 19:58:05 +0300

    s

---
+++ EncapsulationExercise/ShoppingSpree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(value) || value == null)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public List<Product> BagOfProducts
+        {
+            get { return this.bagOfProducts; }
+            set { this.BagOfProducts = value; }
+        }
+    }
+}
---
+++ EncapsulationExercise/ShoppingSpree/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+            var peopleInput = Console.ReadLine().Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            for (int i = 0; i < peopleInput.Length; i++)
+            {
+                var peopleInputSplitted = peopleInput[i].Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                var name = peopleInputSplitted[0];
+                decimal money = decimal.Parse(peopleInputSplitted[1]);
+
+                try
+                {
+                    Person person = new Person(name, money);
+                    people.Add(person);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            var productsInput = Console.ReadLine().Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < productsInput.Length; i++)
+            {
+                var productInputSplitted = productsInput[i].Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                var name = productInputSplitted[0];
+                decimal cost = decimal.Parse(productInputSplitted[1]);
+
+                try
+                {
+                    Product product = new Product(name, cost);
+                    products.Add(product);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            var purchase = "";
+            while ((purchase = Console.ReadLine()) != "END")
+            {
+                var elements = purchase.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    var person = people.Where(p => p.Name.Equals(elements[0])).First();
+                    var product = products.Where(p => p.Name.Equals(elements[1])).First();
+
+                    if (person.Money >= product.Cost)
+                    {
+                        Console.WriteLine($"{person.Name} bought {product.Name}");
+                        people.Where(p => p.Name.Equals(elements[0])).First().Money -= product.Cost;
+                        people.Where(p => p.Name.Equals(elements[0])).First().BagOfProducts.Add(product);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{person.Name} can't afford {product.Name}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+
+
+            }
+            foreach (var person in people)
+            {
+                if (person.BagOfProducts.Count > 0)
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.BagOfProducts.Select(p => p.Name))}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+            }
+        }
+    }
+}
