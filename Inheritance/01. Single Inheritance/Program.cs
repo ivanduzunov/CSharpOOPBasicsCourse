dc6f5e8f331328abbc50e88697b64d698f51d2ef
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-06 18:31:46 +0300

    First three

---
+++ Inheritance/01. Single Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Puppy puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
+    }
+}
+
