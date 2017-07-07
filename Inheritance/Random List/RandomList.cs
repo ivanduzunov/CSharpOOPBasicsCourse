5218912247b46bfbf2b5c62a2263e99f54d050c2
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-07 21:40:04 +0300

    Lab Completed

---
+++ Inheritance/Random List/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class RandomList : ArrayList
+{
+    private Random rnd;
+
+    public RandomList()
+    {
+        this.rnd = new Random();
+    }
+
+    public string RandomString()
+    {
+        return " ";
+    }
+}
+
---
+++ Inheritance/Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public void Push(string element)
+    {
+
+    }
+
+    public string Pop()
+    {
+        return " ";
+    }
+    public string Peek()
+    {
+        return " ";
+    }
+
+    public bool IsEmpty()
+    {
+        return true;
+    }
+}
+
