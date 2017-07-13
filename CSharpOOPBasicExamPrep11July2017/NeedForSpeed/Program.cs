cac06da25a4c02c547f48d77969f9793e7b95752
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-13 17:56:13 +0300

    NFS

--- CSharpOOPBasicExamPrep11July2017/NeedForSpeed/Program.cs
+++ CSharpOOPBasicExamPrep11July2017/NeedForSpeed/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 
 
+
 public class Program
 {
 
@@ -34,11 +35,36 @@ public class Program
                     int durability = int.Parse(tokens[9]);
                     manager.Register(id, carType, brand, model, yearOfProduction, horsepower, acceleration, suspension, durability);
                     break;
+                case "open":
+                    int raceid = int.Parse(tokens[1]);
+                    var raceType = tokens[2];
+                    int length = int.Parse(tokens[3]);
+                    var route = tokens[4];
+                    int prizePool = int.Parse(tokens[5]);
+                    manager.Open(raceid, raceType, length, route, prizePool);
+                    break;
+                case "check":
+                    Console.WriteLine(manager.Check(int.Parse(tokens[1])));                    
+                    break;
+                case "participate":
+                    manager.Participate(int.Parse(tokens[1]), int.Parse(tokens[2]));
+                    break;
+                case "start":
+                    Console.WriteLine(manager.Start(int.Parse(tokens[1])));                                     
+                    break;
+                case "park":
+                    manager.Park(int.Parse(tokens[1]));
+                    break;
+                case "unpark":
+                    manager.Unpark(int.Parse(tokens[1]));
+                    break;
+                case "tune":
+                    int index = int.Parse(tokens[1]);
+                    var addOn = tokens[2];
+                    manager.Tune(index,addOn);
+                    break;
             }
-
         }
-        
-
     }
 
 }
