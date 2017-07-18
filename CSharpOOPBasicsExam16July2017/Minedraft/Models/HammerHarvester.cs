1572d6c5c3775706c643f035efd678774dc8ec2a
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-18 18:34:19 +0300

    w

--- CSharpOOPBasicsExam16July2017/Minedraft/Models/HammerHarvester.cs
+++ CSharpOOPBasicsExam16July2017/Minedraft/Models/HammerHarvester.cs
@@ -10,7 +10,7 @@ public class HammerHarvester : Harvester
     public HammerHarvester(string id, double oreOutput, double energyRequirement)
         : base(id, oreOutput, energyRequirement)
     {
-        this.OreOutput += (base.OreOutput * 200) / 100;
+        this.OreOutput += (this.OreOutput * 200) / 100;
         this.EnergyRequirement *= 2;
     }
 }
